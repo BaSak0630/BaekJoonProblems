cb4a5135e3ec985bdff4df4ff0a4628caa885ce8
BaSak0630 <babamba125@gmail.com>
2024-07-08 23:23:49 +0900

    프로그래머스 짝수 홀수 개수

---
+++ src/EvenAndOddNumbers_Programmers.java
@@ -0,0 +1,21 @@
+public class EvenAndOddNumbers_Programmers {
+    class Solution {
+        public int[] solution(int[] num_list) {
+            int[] answer = new int[2];
+            int sNumCount = 0;
+            int dNumCount = 0;
+            int listLength = num_list.length;
+            for(int i = 0; i < listLength; i++){
+                if(num_list[i] % 2 == 0){
+                    dNumCount++;
+                }
+                if(num_list[i] % 2 != 0){
+                    sNumCount++;
+                }
+            }
+            answer[0] = dNumCount;
+            answer[1] = sNumCount;
+            return answer;
+        }
+    }
+}
